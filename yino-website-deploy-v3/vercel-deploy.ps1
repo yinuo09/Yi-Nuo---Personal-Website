c4d05791c42c86cd6fb523cbdc8bc3c05309e302
yinuo09 <hsun99220@gmail.com>
2026-07-27 12:12:21 +0800

    Delete yino-website-deploy-v3 directory

--- yino-website-deploy-v3/vercel-deploy.ps1
+++
@@ -1,3 +0,0 @@
-$env:VERCEL_TOKEN = "vcp_4PM0E8FXcZUfYMFDsLrKwEB0YRXqY8ZIWTWsOKVh5Y1daijNhZ3cQOz3"
-Set-Location "$env:USERPROFILE\Desktop\yino-website"
-& "C:\Program Files\nodejs\npx.cmd" vercel whoami --token $env:VERCEL_TOKEN
